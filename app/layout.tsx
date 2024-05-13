f742633929969b1228df2c92bc571b8ac35037a1
ejonghwan <sun87-1@daum.net>
2024-05-14 00:13:19 +0900

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,20 +25,13 @@ export const viewport: Viewport = {
 	],
 }
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head />
-			<body
-				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
-				)}
+			<body className={clsx("min-h-screen bg-background font-sans antialiased", fontSans.variable)}
 			>
+				{/* { children } */}
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
 						<Navbar />
