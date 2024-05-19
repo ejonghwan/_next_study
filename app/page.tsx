fa5b2c2449436be8e3b96762cfd52eb8de3618a9
ejonghwan <sun87-1@daum.net>
2024-05-19 23:58:32 +0900

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -75,6 +75,7 @@ export default function Home() {
 		<div>
 			root page
 		</div>
+		// 기존소스 
 		// <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 		// 	<div className="inline-block max-w-lg text-center justify-center">
 		// 		<h1 className={title()}>Make&nbsp;</h1>
